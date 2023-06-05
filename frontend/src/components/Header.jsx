31c02fab4c0aac97566ac3e1526d9b13df1cd279
Srirag Hariharan <sriraghariharan108@gmail.com>
2023-06-05 15:19:03 +0530

    Created a Navbar with 2 navbars. Installed MDBootstrap via npm. Added fontawesome npm package. Styled the navbar to be responsive on mobile as well as in desktop.

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,140 @@
+import React, { useState } from 'react';
+import '../styles/Header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBBtn,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBCollapse,
+} from 'mdb-react-ui-kit';
+import { MDBBadge } from 'mdb-react-ui-kit';
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCartShopping, faUser);
+
+//functional component for header || Navbar
+function Header() {
+  const [showBasic, setShowBasic] = useState(false);
+
+  return (
+    <div>
+        {/* nav1 start */}
+        <div className="header-1">
+            <div className="brand-name">
+                <img width='60px' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-196UvngLppQ7fGE1-TQfG75ZKli2l6IdAkNCgK83g&s" alt="" />
+            </div>
+            <div className="logos">
+                <div className='cart-icon'>
+                    <FontAwesomeIcon icon={faCartShopping} size="lg" /> 
+                    <MDBBadge pill color='dark' light><b>999</b></MDBBadge>
+                </div>
+                <div>
+                <MDBDropdown group className='shadow-0'>
+                    <MDBDropdownToggle color='light'>
+                        <FontAwesomeIcon icon={faUser} size="xl" />
+                        <MDBBadge className='p-3' pill color='success' light><b>Hariharan</b></MDBBadge>
+                    </MDBDropdownToggle>
+                    <MDBDropdownMenu style={{ margin: 0 }}>
+                        <MDBDropdownItem link>Hi Srirag Hariharan</MDBDropdownItem>
+                        <MDBDropdownItem divider />
+                        <MDBDropdownItem link>Update Profile</MDBDropdownItem>
+                        <MDBDropdownItem link>Logout</MDBDropdownItem>
+                    </MDBDropdownMenu>
+                </MDBDropdown>
+                </div>
+               
+        {/* nav 2 start */}
+      <div>
+            </div>
+        </div>
+            
+        </div>
+
+        {/* nav2 start */}
+        <div className="header-2">
+            <MDBNavbar expand='md' light className='header-2 p-4'>
+            <MDBContainer fluid>
+                <MDBNavbarToggler
+                aria-controls='navbarSupportedContent'
+                aria-expanded='false'
+                aria-label='Toggle navigation'
+                onClick={() => setShowBasic(!showBasic)}
+                >
+                <MDBIcon icon='bars' fas />
+                </MDBNavbarToggler>
+                <MDBCollapse navbar show={showBasic}>
+                <MDBNavbarNav className='mr-auto mb-2 mb-lg-0 d-flex justify-content-center'>
+                    <MDBNavbarLink> Home             </MDBNavbarLink>
+                    <MDBNavbarLink> All Products     </MDBNavbarLink>
+                    <MDBNavbarLink> Best selling     </MDBNavbarLink>
+
+                    <MDBDropdown>
+                        <MDBDropdownToggle tag='a' className='nav-link' role='button'>
+                        Products
+                        </MDBDropdownToggle>
+                        <MDBDropdownMenu style={{ margin: 0 }}>
+                        <MDBDropdownItem link>English willow bats                   </MDBDropdownItem>
+                        <MDBDropdownItem link>Kashmir willow bats                   </MDBDropdownItem>
+                        <MDBDropdownItem link>Batting Pads                          </MDBDropdownItem>
+                        <MDBDropdownItem link>Helmets                               </MDBDropdownItem>
+                        <MDBDropdownItem link>Leather balls                         </MDBDropdownItem>
+                        <MDBDropdownItem link>Elbow guards                          </MDBDropdownItem>
+                        <MDBDropdownItem link>Batting gloves                        </MDBDropdownItem>
+                        <MDBDropdownItem link>Chest Guards                          </MDBDropdownItem>
+                        <MDBDropdownItem link>Abdomen Guards                        </MDBDropdownItem>
+                        <MDBDropdownItem link>Thigh Guards                          </MDBDropdownItem>
+                        <MDBDropdownItem link>Cricket shoes                         </MDBDropdownItem>
+                        <MDBDropdownItem link>Wicket keeping pads                   </MDBDropdownItem>
+                        <MDBDropdownItem link>Wicket keeping gloves                 </MDBDropdownItem>
+                        <MDBDropdownItem link>Fielding Pads                         </MDBDropdownItem>
+                        <MDBDropdownItem link>Tennis Balls                          </MDBDropdownItem>
+                        {/* <MDBDropdownItem link>                  </MDBDropdownItem>
+                        <MDBDropdownItem link>                  </MDBDropdownItem>
+                        <MDBDropdownItem link>                  </MDBDropdownItem>
+                        <MDBDropdownItem link>                  </MDBDropdownItem> */}
+                        </MDBDropdownMenu>
+                    </MDBDropdown>
+
+                    <MDBDropdown>
+                        <MDBDropdownToggle tag='a' className='nav-link' role='button'>
+                        Categories
+                        </MDBDropdownToggle>
+                        <MDBDropdownMenu style={{ margin: 0 }}>
+                        <MDBDropdownItem link>Batting               </MDBDropdownItem>
+                        <MDBDropdownItem link>Bowling               </MDBDropdownItem>
+                        <MDBDropdownItem link>Wicket Keeping        </MDBDropdownItem>
+                        <MDBDropdownItem link>Fielding              </MDBDropdownItem>
+                        <MDBDropdownItem link>Umpire Acessories     </MDBDropdownItem>
+                        <MDBDropdownItem link>Coach Acessories      </MDBDropdownItem>
+                        <MDBDropdownItem link>Others                </MDBDropdownItem>
+                        </MDBDropdownMenu>
+                    </MDBDropdown>
+
+                     <MDBNavbarLink> FAQ     </MDBNavbarLink>
+                </MDBNavbarNav>
+
+                {/* <form className='d-flex input-group w-auto'>
+                    <input type='search' className='form-control' placeholder='Type query' aria-label='Search' />
+                    <MDBBtn color='primary'>Search</MDBBtn>
+                </form> */}
+                </MDBCollapse>
+            </MDBContainer>
+            </MDBNavbar>
+        </div>
+    </div>
+  );
+}
+
+export default Header
