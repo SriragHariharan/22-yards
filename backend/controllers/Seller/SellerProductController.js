6948a38b3b39632ae5a74711f6ba3a5512afa028
Srirag Hariharan <sriraghariharan108@gmail.com>
2023-06-07 00:08:16 +0530

    Added two routes on server for seller to view products & delete products.

--- backend/controllers/Seller/SellerProductController.js
+++ backend/controllers/Seller/SellerProductController.js
@@ -1,6 +1,9 @@
 //add a new product for sales by the seller
 const Products = require("../../models/ProductsModel");
 
+const uploadPath ='./uploads/product-images/'       
+const fs = require("fs")
+
 const AddNewProduct = async(req, res) => {
     try {
         let sellerID = req.sellerID
@@ -11,7 +14,6 @@ const AddNewProduct = async(req, res) => {
         let newProduct = new Products({ sellerID, brand, productName, features, originalPrice, offerPrice, stock, size });
         let productFromDB = await newProduct.save()
         //file upload
-        uploadPath ='./uploads/product-images/'       
 
         let image1 = req.files.image1;
         let image2 = req.files.image2;
@@ -28,7 +30,46 @@ const AddNewProduct = async(req, res) => {
     }
 }
 
+//get all products for a specific seller
+const getProducts = async(req, res) => {
+    try {
+        let sellerID = req.sellerID;
+        let products = await Products.find({sellerID})
+        return res.json({ success:true, message:"Data fetched", data:{products} })
+    } catch (error) {
+        return res.json({ success:false,error_code:400, message:error.message, data:{} })
+    }
+}
+
+const deleteProduct = async(req, res) => {
+    try {
+        let productID = req.params.id;
+        let product = await Products.findOne({_id:productID})
+        if(product === null){
+            return res.json({ success:false, message:"Unable to find product", error_code:500, data:{} })
+        }
+        if(product.sellerID !== req.sellerID){
+            return res.json({ success:false, message:"Unable to delete", error_code:500, data:{} })
+        }
+        let deletedResponse = await Products.deleteOne({_id:productID})
+        if(deletedResponse.deletedCount === 0){
+            return res.json({ success:false, message:"Nothing to delete", error_code:500, data:{} })            
+        }
+        fs.unlinkSync(uploadPath + product._id +"-01.jpg");
+        fs.unlinkSync(uploadPath + product._id +"-02.jpg");
+        fs.unlinkSync(uploadPath + product._id +"-03.jpg");
+        return res.json({ success:true, message:"Product deleted successfully", data:{} })            
+    } 
+    catch (error) {
+        return res.json({ success:false, message:error.message, error_code:500, data:{} })            
+    }
+}
+
+
+
 module.exports = {
     AddNewProduct,
+    getProducts,
+    deleteProduct
 }
 
--- backend/routes/SellerRouter.js
+++ backend/routes/SellerRouter.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 
 const { AddNewSeller, LoginSeller } = require('../controllers/Seller/SellerAuthController');
+const { deleteProduct } = require('../controllers/Seller/SellerProductController');
+const { getProducts } = require('../controllers/Seller/SellerProductController');
 const { AddNewProduct } = require('../controllers/Seller/SellerProductController');
 
 const SellerAuthMiddleware = require('../middlewares/auth.seller');
@@ -13,6 +15,12 @@ router.post('/auth/login', LoginSeller )
 
 //add new product to sales
 router.post('/add-new-product/', SellerAuthMiddleware, AddNewProduct  )
+
+//get all products
+router.get('/', SellerAuthMiddleware, getProducts  )
+
+//delete a product
+router.delete('/delete-product/:id', SellerAuthMiddleware, deleteProduct )
 
 
 module.exports = router
