5c3c5f6d1a8c95d10dd1aa887a2f53df1e238e78
Srirag Hariharan <sriraghariharan108@gmail.com>
2023-06-17 11:12:08 +0530

    Added backend API to sort products by date and price.

--- backend/controllers/Buyer/BuyerProductsController.js
+++ backend/controllers/Buyer/BuyerProductsController.js
@@ -37,7 +37,35 @@ const getSingleProduct = async(req, res) => {
 }
 
 
+//sort by date
+const sortByDate = async(req, res) => {
+    try {
+        const id = req.params.id
+        const products = await Products.find({}).sort({createdAt:id});
+        return res.json({  success:true, message:"Data sorted successfully", data:{products}})
+    } catch (error) {
+        return res.json({  success:false, data:error.message, error_code:404, data:{} })
+    }
+}
+
+//sort by price
+const sortByPrice = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const products = await Products.find({}).sort({price:id})
+        return res.json({ success:true, message:"Product sorted by price", data:{products} }) 
+    } 
+    catch (error) {
+        return res.json({ success:false, message:error.message, error_code:404, data:{} })
+    }
+}
+
+
+
+
 module.exports={
     getAllProducts,
     getSingleProduct,
+    sortByDate,
+    sortByPrice,
 }
